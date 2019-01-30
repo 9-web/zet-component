058c34132a6ff927d86665d2fdb66229d9b37dd2
jiechud <qiaojienet@163.com>
2019-01-30 16:05:22 +0800

    tag input components

---
+++ packages/zet-component/src/automl/algorithm/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface AlgorithmProps {
+}
+
+export default class Algorithm extends React.Component<AlgorithmProps, any> {
+  public render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
--- packages/zet-component/src/index.tsx
+++ packages/zet-component/src/index.tsx
@@ -6,3 +6,6 @@ export { default as LocaleProvider} from './components/locale-provider';
 
 /** AutoML */
 export { default as AutoML} from './automl';
+
+/** 标签输入 */
+export { default as TagInput } from './components/tag-input';
