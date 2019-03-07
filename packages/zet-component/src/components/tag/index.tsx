cea98f6853ca1a5eb9e4d57fabf202006c589805
lmh0 <987156088@qq.com>
2019-03-07 18:08:52 +0800

    Newtag (#21)
    tag

---
+++ packages/zet-component/src/components/tag/index.tsx
@@ -0,0 +1,143 @@
+import * as React from 'react';
+import { Dropdown, Button, Icon, Checkbox, Input, Badge, Empty } from 'antd';
+import classnames from 'classnames';
+import TagsInput from './tags-input.js'
+import styles from './index.less';
+import { emptyPicture } from '../../utils/utils';
+
+export interface Tags {
+  title: string,
+  key: string,
+  value: string,
+}
+
+export interface TagProps {
+  /** 组件行行内样式 */
+  style?: React.CSSProperties,
+  /** 自定义类名 */
+  className?: string,
+  /** 是否禁用 */
+  disabled?: boolean,
+  /** 输入框内容变化时的回调 */
+  onChange?: (value) => {},
+  /** 下拉框数据 */
+  data: Array<Tags>,
+  /** 按钮图标 */
+  icon?: string,
+  /** 占位符 */
+  placeholder?: string,
+}
+
+class Tag extends React.Component<TagProps, any> {
+
+  constructor(props: TagProps) {
+    super(props);
+  }
+
+  static defaultProps = {
+    data: [],
+    icon: "filter",
+    placeholder: '请选择标签'
+  }
+
+  state = {
+    visible: false,
+    selectNums: [],
+    delData: [],
+    inputValue: undefined,
+  }
+
+  handleVisibleChange = (flag) => {
+    this.setState({ visible: flag })
+  }
+
+  selectNums = (keys) => {
+    const { data, onChange } = this.props;
+    this.setState({ selectNums: keys });
+    const difference = Array.from(new Set([...this.state.selectNums].filter(x => !new Set(keys).has(x))));
+    this.setState({ delData: difference });
+    const filterArr = Array.from(new Set(data.filter(x => new Set(keys).has(x.title))));
+    onChange && onChange(filterArr);
+    this.onInput('');
+  }
+
+  delTag =  (tag) => {
+    const { data, onChange } = this.props;
+    this.setState(prevState => {
+      let { selectNums, delData } = prevState;
+      const index = selectNums.indexOf(tag);
+      if (index != -1) {
+        const elem = selectNums.splice(index, 1);
+        delData = elem;
+      }
+      const filterArr = Array.from(new Set(data.filter(x => new Set(selectNums).has(x.title))));
+      onChange && onChange(filterArr);
+      return { selectNums, delData };
+    });
+  }
+
+  onInput = (value) => {
+    this.setState({inputValue:value});
+  }
+
+
+  render() {
+    const { data, style, className, icon, placeholder } = this.props;
+    const { selectNums, delData, visible, inputValue } = this.state;
+    const styleProps = {
+      width: 39,
+      ...style,
+    }
+    const classNames = classnames(styles.zetTag, className);
+    let arr = [];
+    if (data && data.length > 0) {
+      if (inputValue) {
+        arr = data.filter(v=>v.title.toUpperCase().includes(inputValue.toUpperCase()));
+      } else {
+        arr = data;
+      }
+    }
+    const options = arr.length > 0 && arr.map((v) => {
+      return {
+        label: <span key={v.key}>{v.title}</span>,
+        value: v.title,
+      }
+    });
+    return (
+      <div style={{ display: 'inline-block' }}>
+        <Dropdown
+          overlay={
+            <div
+              className={styles.zetTagSetdrop}
+              onClick={() => { this.handleVisibleChange(true); }}
+            >
+              <div className={styles.zetTagInput}>
+                <TagsInput placeholder={placeholder} addData={selectNums} delData={delData} onChange={this.delTag} onInput={this.onInput} inputValue={inputValue} />
+              </div>
+              {options && options.length > 0 ? (
+                <Checkbox.Group
+                  onChange={this.selectNums}
+                  style={{ width: '100%', zIndex: 1000 }}
+                  value={selectNums}
+                  options={options}
+                  className={styles.zetTagSettableChecklist}
+                >
+                </Checkbox.Group>) : (
+                <Empty className={styles.empty} description='暂无数据' image={<img style={{ width: 63, height: 40 }} src={emptyPicture()} />} />
+                )
+              }
+            </div>
+          }
+          visible={visible}
+          trigger={['click']}
+          placement='bottomCenter'
+          onVisibleChange={this.handleVisibleChange}
+        >
+          <Badge dot={selectNums.length > 0} offset={[-12,8]}><Button className={classNames} style={styleProps} icon={icon} /></Badge>
+        </Dropdown>
+      </div>
+    )
+  }
+}
+
+export default Tag;
---
+++ packages/zet-component/src/components/tag/tags-input.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { Input, Tag } from 'antd';
+import styles from './index.less';
+
+class TagsInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const valuelist = new Set(props.value) || new Set();
+    this.input = React.createRef();
+    this.state = {
+      valuelist,
+      focus: false,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if ('value' in nextProps) {
+      const valuelist = new Set(nextProps.value) || new Set();
+      this.setState({ valuelist });
+    }
+  }
+
+  onInputChange = (e) => {
+    const { onInput } = this.props;
+    const {value} = e.target;
+    // const reg = /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/;
+    if ((!Number.isNaN(value)) || value === '' || value === '-') {
+      onInput && onInput(value);
+    }
+  }
+
+  // onPressEnter = (e) => {
+  //   const { value } = e.target;
+  //   if (value.length !== 0) {
+  //     // console.log('value', value);
+  //     this.setState(prevState => {
+  //       const { valuelist } = prevState;
+  //       valuelist.add(value);
+  //       this.triggerChange([...valuelist]);
+  //       return { valuelist, inputvalue: undefined };
+  //     });
+  //   }
+  // }
+
+  onInputBlur = (e) => {
+    const { onBlur } = this.props;
+    // if (e.target.value) {
+    //   this.onPressEnter(e);
+    // }
+    if (onBlur) {
+      this.setState({ focus: false });
+      onBlur();
+    }
+  }
+
+  removeTag = (v, e) => {
+    e.preventDefault();
+    const { disabled } = this.props;
+    if (!disabled) {
+      this.setState(prevState => {
+        const { valuelist } = prevState;
+        valuelist.delete(v);
+        this.triggerChange(v);
+        return { valuelist };
+      });
+    }
+  }
+
+  triggerChange = (changedValue) => {
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange(changedValue);
+    }
+  }
+
+  onDivClick = () => {
+    this.input.current.focus();
+  }
+
+  onInputFocus = () => {
+    this.setState({ focus: true });
+  }
+
+  render() {
+    const { valuelist, focus } = this.state;
+    const { disabled, addData, delData, inputValue, placeholder } = this.props;
+    addData.length > 0 && addData.forEach(v => {
+      valuelist.add(v);
+    });
+    delData.length > 0 && delData.forEach(v => {
+      valuelist.delete(v);
+    });
+    const rendervalue = [];
+    valuelist.forEach(v => {
+      rendervalue.push(
+        <li key={v}>
+          <Tag
+            closable
+            onClose={(e) => this.removeTag(v, e)}
+            key={v}
+            color="#f5f5f5"
+            className={styles.tagsinputtag}
+          >
+            {v}
+          </Tag>
+        </li>
+      );
+    });
+    return (
+      <div className={`${styles.tagsinput} ${focus ? styles.tagsinputfocus : ''} ${disabled ? styles.tagsinputdisabled : ''} `} style={{ width: '100%' }} onClick={this.onDivClick}>
+        <ul>
+          {rendervalue}
+          <li key='input'>
+            <Input
+              className='ant-select-search__field'
+              onChange={this.onInputChange}
+              onBlur={this.onInputBlur}
+              value={inputValue}
+              // maxLength='25'
+              // onPress={this.onPressEnter}
+              ref={this.input}
+              onFocus={this.onInputFocus}
+              disabled={disabled}
+              placeholder={valuelist.size === 0 ? placeholder : ''}
+            />
+          </li>
+        </ul>
+        {disabled && <div className='disabled' />}
+      </div>
+    );
+  }
+}
+
+export default TagsInput;
